0bdb6ba359bca3bb037235390eec657744a88707
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-08-11 03:03:42 +0530

    area_triangle

---
+++ area_triangle.py
@@ -0,0 +1,9 @@
+# program to find area of triangle a = 1/2 * b * h
+def area_triangle():
+    breadth = int(input('enter the breadth:'))
+    height = int(input('enter the height:'))
+    result = 1/2 * breadth * height 
+    print(result)
+    
+
+area_triangle()
