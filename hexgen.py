69dd169e2832a7ceba51879e0799d5a280c88f28
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-08-28 04:34:18 +0530

    text_in

--- hexgen.py
+++ hexgen.py
@@ -3,7 +3,7 @@ import random
 def hex_generator(length = 6):
   
   if length <= 0:
-    raise ValueError("Length can't b less than 6")
+    raise ValueError("Length can't be less than 6")
   
   hex_characters = "0123456789ABCDEF"
   hexcode = "".join(random.choice(hex_characters) for _ in range(length))
@@ -14,3 +14,4 @@ if __name__ == "__main__":
   hexcode_length = int(input('enter the length: '))
   generated_hexcode = hex_generator(hexcode_length)
   print(f"Generated hexcode:{generated_hexcode}")
+
