4dfb602f57930fc6122ee116d366f29bac32a4d4
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-08-31 08:31:52 +0530

    updated_code

--- bill_gen.py
+++ bill_gen.py
@@ -7,13 +7,17 @@ def calculate_total_cost(menu):
         if user_input in menu:
             try:
                 user_selection = int(input(f'Enter how many {user_input}(s): '))
-                total_cost += menu[user_input] * user_selection
-                print(f'Added {user_selection} {user_input}(s) to your order.')
+                if user_selection > 6:
+                    print("Enter selection's not more than 6")
+                else:
+                    total_cost += menu[user_input] * user_selection
+                    print(f'Added {user_selection} {user_input}(s) to your order.')
+                
             except ValueError:
                 print('Please enter a valid number for the quantity.')
         else:
             print('ERROR: There is no such item in the menu.')
-    print(f'Total cost of your order: {total_cost}')
+    print(f'Total cost of your order: ${total_cost}')
 
 menu = {'wada': 3, 'idly': 2, 'dosa': 5}
 calculate_total_cost(menu)
