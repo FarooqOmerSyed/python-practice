a0af96b151cd5a192f9e2a5fbf6f73e9cf9feda4
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-08-31 08:38:10 +0530

    multiplication_table_gen

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,6 @@
 def multi_table():
     user_input = int(input('Enter your number: '))
     for i in range(1, 11):
-        print(f"{user_input} * {i} = {user_input * i}")
+        print(f"{user_input} x {i} = {user_input * i}")
 
 multi_table()
